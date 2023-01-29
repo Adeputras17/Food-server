309ccfee7f80481a26bfc39bd065ff492729a146
Adeputras17 <kirabelkparg@gmail.com>
2023-01-29 22:05:30 +0800

    update

--- server.js
+++ server.js
@@ -39,11 +39,7 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/client/build/index.html'))
-);
+
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
